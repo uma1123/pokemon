87a8060e0170e3834e8b38f247a38e45cf27b8fe
uma1123 <yuma1596@gmail.com>
2025-04-14 16:59:00 +0900

    feat: カードコンポーネントのスタイルを改善し、タイプバッジを追加

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
+import { getTypeColor } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
 
 interface CardProps {
   id: number;
@@ -11,12 +13,13 @@ interface CardProps {
 }
 
 export default function Card({ id, name, image, type, className }: CardProps) {
+  console.log(className);
   return (
     <Link href={`/pokemon/${id}`}>
       <div
-        className={`${className} flex flex-col items-center justify-center py-2 mr-1 border border-gray-300 rounded-md min-w-40 text-center shadow-md shadow-black hover:scale-110 transition-transform duration-300 cursor-pointer`}
+        className={`flex flex-col items-center justify-center py-2 mr-1 border border-gray-300 rounded-md min-w-40 text-center shadow-md shadow-black hover:scale-110 transition-transform duration-300 cursor-pointer`}
       >
-        <div className="rounded-2xl py-2 px-1 bg-white bg-opacity-30">
+        <div className="rounded-2xl py-2 px-1 ">
           <Image
             src={image}
             alt={name}
@@ -24,8 +27,17 @@ export default function Card({ id, name, image, type, className }: CardProps) {
             height={120}
             className="w-[120px] h-[120px]"
           />
-          <h4>{name}</h4>
-          <h3 className="mb-1">{type}</h3>
+          <h4 className="font-bold text-lg text-gray-800">{name}</h4>
+          <div className="flex justify-center mt-2">
+            <Badge
+              className={`${getTypeColor(
+                type
+              )} px-3 py-1 text-sm rounded-full text-white`}
+              style={{ backgroundColor: "initial" }}
+            >
+              {type}
+            </Badge>
+          </div>
         </div>
       </div>
     </Link>
